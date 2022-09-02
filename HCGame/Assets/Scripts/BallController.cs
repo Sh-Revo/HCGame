2fab82a0eac20b16946414fecef3d7001943b550
Sh-Revo <shandov@i.ua>
2022-09-02 22:27:02 +0300

    make ball controller

--- HCGame/Assets/Scripts/BallController.cs
+++ HCGame/Assets/Scripts/BallController.cs
@@ -4,30 +4,54 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    [SerializeField] private LineRenderer[] lineRenderers;
-    [SerializeField] private Transform[] stripPositions;
-    [SerializeField] private Transform center;
-    [SerializeField] private Transform ballPosition;
-
-    bool isMouseDown;
+    [SerializeField] private float startSpeed;
+    private float currentSpeed;
+    private int interpolationFramesCount = 45; // Number of frames to completely interpolate between the 2 positions
+    private float value = 100;
+    bool isMouseDown = false;
 
     void Start()
     {
-        lineRenderers[0].positionCount = 2;
-        lineRenderers[1].positionCount = 2;
-        lineRenderers[0].SetPosition(0, stripPositions[0].position);
-        lineRenderers[1].SetPosition(0, stripPositions[1].position);
+        currentSpeed = startSpeed;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        Move();
+        //if (isMouseDown)
+        //{
+
+        //}
+        //else
+        //{
+        //}
+        value -= Time.deltaTime * 5;
+        while (value > 0)
+        {
+            transform.position += transform.forward * currentSpeed * Time.deltaTime;
+            currentSpeed /= 10;
+            Debug.Log("Zero");
+        }
     }
 
-    private void Move()
+    private void OnMouseDown()
     {
+        isMouseDown = true;
+    }
 
+    private void OnMouseUp()
+    {
+        isMouseDown = false;
+        //currentSpeed = startSpeed;
+        //Move();
+    }
+
+    void Move()
+    {
+        while (currentSpeed > 0)
+        {
+            transform.position += transform.forward * currentSpeed * Time.deltaTime;
+            currentSpeed /= 10;
+        }
+        Debug.Log("Speed = " + currentSpeed);
     }
 }
