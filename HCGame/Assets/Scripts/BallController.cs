7f175fcf9c30d027c9a58bde652eeacf58ae58e5
Sh-Revo <shandov@i.ua>
2022-09-02 14:08:11 +0300

    Create field, ball, camera controller

---
+++ HCGame/Assets/Scripts/BallController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private LineRenderer[] lineRenderers;
+    [SerializeField] private Transform[] stripPositions;
+    [SerializeField] private Transform center;
+    [SerializeField] private Transform ballPosition;
+
+    bool isMouseDown;
+
+    void Start()
+    {
+        lineRenderers[0].positionCount = 2;
+        lineRenderers[1].positionCount = 2;
+        lineRenderers[0].SetPosition(0, stripPositions[0].position);
+        lineRenderers[1].SetPosition(0, stripPositions[1].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        Move();
+    }
+
+    private void Move()
+    {
+
+    }
+}
